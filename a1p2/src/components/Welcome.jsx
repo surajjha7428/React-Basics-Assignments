f66c15046ef8e6591f504c009ea6c50ab853a46e
surajjha7428 <143312238+surajjha7428@users.noreply.github.com>
2024-05-23 01:13:13 +0530

    Done

---
+++ a1p2/src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+// src/components/Welcome.jsx
+
+import React from "react";
+
+function Welcome() {
+  const name = "React Developer";
+  return (
+    <div>
+      <p>
+        Welcome, {/* complete the missing code */} {name}!
+      </p>
+    </div>
+  );
+}
+
+export default Welcome;
